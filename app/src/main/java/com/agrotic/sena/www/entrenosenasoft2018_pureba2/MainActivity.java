117daba298dff37a878da3459d9ca081c1a7f9c6
jos3cort3s20 <j_cortes20@hotmail.com>
2018-07-24 17:05:43 -0500

    Mensaje
    Signed-off-by: jos3cort3s20 <j_cortes20@hotmail.com>

--- app/src/main/java/com/agrotic/sena/www/entrenosenasoft2018_pureba2/MainActivity.java
+++ app/src/main/java/com/agrotic/sena/www/entrenosenasoft2018_pureba2/MainActivity.java
@@ -2,15 +2,22 @@ package com.agrotic.sena.www.entrenosenasoft2018_pureba2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    /* hola mundo */
+    public void agregaId(){
+
+    }
+
 
 }
